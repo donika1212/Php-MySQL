33209680c48be690910e958fa2d518415ffb27a4
Amar-Simnica <amarsimnica15@gmail.com>
2024-11-02 13:09:59 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,114 +1,75 @@
 <?php
 
-$age = 28;
-switch($age){
-    case($age >=8 && $age <18);
-    echo"You are a minor(8-18 years old)" ."<br>";
-    break;
-    case($age >= 18 && $age <=25);
-    echo"You are a young adult" . "<br>";
-    break;
+$school = "Digital school";
+echo "I love $school"  . "<hr>";
 
-    case($age > 25);
-    echo"You are an adult" ."<br>";
-    break;
-    default;
-    echo"Invalid age" ."<br>";
-    break;
-}
+$x = 120;
+$y = 50;
 
-$day = "Tuesday";
-switch($day) {
-    case "Monday":
-        echo"Its Monday!" ."<br>";
-        break;
+echo $x + $y . "<br>";
+echo $x - $y  . "<br>";
 
-        case"Tuesday";
-        echo"It's Tuesday" . "<br>";
-        break;
+echo $x / $y  .  "<br>";
+echo $x * $y  .  "<br>";
 
-        case"Wednesday";
-        echo"It's Wednesday" . "<br>";
-        break;
+echo $x % $y . "<br>";
 
-        case"Thursday";
-        echo"It's Thursday" . "<br>";
-        break;
 
-        case"Friday";
-        echo"It's Friday" . "<br>";
-        break;
+$a="Digital";
+$b="School";
 
-        case"Saturday";
-        echo"It's Saturday" . "<br>";
-        break;
+$c = $a.$b;
 
-        case"Sunday";
-        echo"It's Sunday" . "<br>";
-        break;
+echo $c ."<br>";
 
-        default;
-        echo"Invalid day" . "<br>";
-        break;
+$the_string = "Digital School";
+
+echo strlen($the_string) . "<br>";
+
+echo str_word_count($the_string) . "<br>";
+
+$programming = "Programing is not cool";
+echo str_replace("not","very",$programming) . "<br>";
+
+$the_string = "Programming";
+echo strrev($the_string) . "<br>";
+
+//
+$num = -1;
+
+if($num < 0){
+    echo"Nun is less than 0" . "<br>";
 }
 
 
-
-$x = 1;
-
-while($x <= 5){
-    echo "The number is : $x". "<br>";
-    $x++;
-}
-$y = 1;
-
-do {
-    echo"The number is $y" ."<br>";
-    $y++;
-}while($y <= 6);
-
-for($x = 0;$x<=10;$x++){
-    echo"Shkolla digjitale $x"."<br>";
-
-}
-$cars = array("BMW","W","Audi","Tesla");
-
-foreach($cars as $value){
-    echo"$value" . "<br>";
+$age = 15;
+if(($age > 12) &&($age < 20)){
+    echo "You are a teenager" . "<br>";
 }
 
-$age = array("John" => "18", "Michael" => "23","Joe"=>"18");
-
-foreach($age as $key => $value){
-    echo"$key = $value"."<br>";
+if($age < 18){
+    echo "You are under 18" . "<br>";
+} else{
+    echo "You are an adult";
 }
 
-// BuildFunctions
-
-
-//phpinfo();
-
-$y = "Helloooo" . "<br>";
-
-print_r($y);
-
-$x =2;
-echo gettype($x) . "<br>";
-
-$x =2.3;
-echo gettype($x) . "<br>";
-
-$x ="Shkolla";
-echo gettype($x) . "<br>";
-
-function displayPhpVersion (){
-    echo"This is PHP" . phpversion() . "<br>";
+if($num < 0){
+    echo"The value of $num is a negative number" . "<br>";
 }
-displayPhpVersion();
+else if($num ==0){
+    echo"The value of $num is 0";
+} else{
+        echo"The value of $num is a positive number";
+}
 
-//User defined functions
+ $x = 20;
+ $y = 14;
 
-
+ if($x == $y){
+    echo"Yes";
+ } else{
+    echo"No";
+ }
 
 
 ?>
