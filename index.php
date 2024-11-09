f1564d3052a2bac48970cf049b6a80221c5ac9ae
frasher165 <frashergashi5@gmail.com>
2024-11-09 14:46:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,123 +1,157 @@
 <?php
 
-function maximum($x,$y){
 
-  if($x > $y){
-    return $x;
-}else{
-    return $y;
+
+$burgaxhit = array(
+    array("Hashimi","Hage",28),
+    array("Kadri","Hage",15),
+    array("Jakupi","Hage",10),
+);
+
+echo $burgaxhit[0][0] . ":ORIGIN:" . $burgaxhit[0][1] . ":Lifesentence:" . $burgaxhit[0][2] . "<br>";
+echo $burgaxhit[1][0] . ":ORIGIN:" . $burgaxhit[1][1] . ":Lifesentence:" . $burgaxhit[1][2] . "<br>";
+echo $burgaxhit[2][0] . ":ORIGIN:" . $burgaxhit[2][1] . ":Lifesentance:" . $burgaxhit[2][2] . "<br>";
+
+for($row = 0 ;$row<3;$row++){
+echo "<p><b> row number $row </b></p>";
+echo "<ul>";
+for($col=0;$col<3;$col++){
+    echo "<li>" . $burgaxhit[$row][$col]. "</li>";
+
+}
+echo "</ul>";
+
+///////////////////////////////////////////////////////////////
+
+$iphones = array(
+    array("iPhone 12", "Apple", 799),
+    array("iPhone 13", "Apple", 899),
+    array("iPhone 14", "Apple", 999),
+    array("iPhone 15", "Apple", 1099), 
+);
+
+
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Model</th><th>Brand</th><th>Price</th></tr>"; 
+
+
+for ($row = 0; $row < 4; $row++) { 
+    echo "<tr>"; 
+    for ($col = 0; $col < 3; $col++) { 
+        echo "<td>" . $iphones[$row][$col] . "</td>"; 
+    }
+    echo "</tr>"; 
+}
+echo "</table>"; 
+
+}
+//nerted loops
+///////////////////////////////////////////////////////////////
+
+$arrays = array(
+    array(1,2,3),
+    array(1,2,3),
+    array(1,2,3),
+);
+
+for($i=0; $i<3; $i++){
+
+    for($j=0; $j<3; $j++){
+
+        echo "array: $i element: $j .<br> ";
+    }
 }
 
+for($i=0; $i < 5; $i++){
+
+    for($j=0; $j<$i; $j++){
+echo "*";
+
+    }
+    echo "<br>";
 }
 
-$a = 20;
-$b = 30;
+
+$grade = array(
+    "math" => "1",
+    "art" => "5",
+    "history" => "8",
+    "Gjuh shqip" => "3",
+);
+
+echo "math grade is" .$grade["math"];
+
+
+foreach($grade as $subject => $grade){
+
+    echo "subject : " .$subject . ", grade :"  .$grade;
+    echo "<br>";
+}
+
+
+
+
+
+
  
 
-$test = maximum($a,$b);
-echo "the maximun of $a and $b is $test". "<br>";
 
 
 
-function fully_divisible($n)  {
-     
-    if ($n % 2 == 0) {
-        return "$n is fully divisible by 2";
-    }else
-    return"$n is not fully divisible by 2 ";
-  
-   } 
-    //print_r(fully_divisible(4)). "<br>";
 
 
-$x = 5;
-
-function localVariable(){
-global $x;
-$y=10;// local
-echo $x ."<br>";
-echo $y ."<br>";
-
-}
-localVariable();
 
 
-function add1(){
-    static $number=0;
-$number++;
-return $number;
-}
-
-echo add1();
-echo "<br>";
-echo add1();
-echo "<br>";
-echo add1();
 
 
-$sport1 = "football";
-$sport2 = "basketboll";
-$spoet3 = "handball";
-$sport4 = "volleyball";
 
 
-$sports = array("football","basketball","handball","volleyball");
-echo "<br>";
-echo $sports[0];
-echo "<br>";
-echo $sports[2];
-echo "<br>";
-echo end($sports);
-echo "<br>";
-echo count($sports);
-echo "<br>";
-
-$length = count($sports);
-for($i = 0; $i < $length; $i++){
-    echo $sports[$i] ,"\n";
-}
-echo "<br>";
-array_push($sports, "golf 7 gti");
-
-var_dump($sports);
-echo "<br>";
-
-array_pop($sports);
-var_dump($sports);
-
-array_unshift($sports, "tennis");
-var_dump($sports);
-echo "<br>";
-
-array_shift($sports);
-var_dump($sports);
-echo "<br>";
-
-$output = array_slice($sports, 2);
-var_dump($output);
-echo "<br>";
-
-$sports = ["football","basketball","handball","volleyball"];
-
-$output = array_slice($sports ,0, 3);
-var_dump($output);
-echo "<br>";
 
 
-$sports = ["football","basketball","handball","volleyball"];
-$output = array_slice($sports ,-2, 2);
-var_dump($output);
-echo "<br>";
 
 
-$values =  [12,24,30,55];
 
-$sum = array_sum($values);
-var_dump($sum);
-echo "<br>";
 
-$week_temperature = [22,34,40,10,49,54,33];
-$average = array_sum($week_temperature)/7;
-echo($average);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
