db6aa1d9f79ca772bb087b50b2aba7fe1af2e847
AjanSs <ajansyla2@gmail.com>
2024-11-30 14:44:50 +0100

    Add files via upload

---
+++ module7/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+
+try {
+
+    $conn = new PDO ("mysql:host=$host", $user, $pass);
+
+    $sql = "CREATE DATABASE database1";
+
+    $conn -> exec($sql);
+
+    echo "Database is created";
+
+}catch (Exeption $e) {
+    echo "Database is not created";
+
+
+}
+
+?>
