a88317726a9c0752c88b8859d44bcfa7bfc56946
aldini212 <aldinihalimi@gmail.com>
2024-11-30 14:09:14 +0100

    Add files via upload

---
+++ module-6/index.php
@@ -0,0 +1,53 @@
+<?php 
+
+//fopen
+$my_file = fopen("file1.txt", "w");
+
+//fclose
+fclose($my_file);
+
+//fread
+
+$filename = "ds.txt";
+
+$file = fopen($filename,"r");
+
+$filesize = filesize($filename);
+
+$my_filedata = fread ($file,$filesize);
+
+echo $my_filedata . "<br>";
+
+fclose($file);
+
+//feof 
+
+$file1 = fopen("example.txt", "r");
+
+while(!feof($file1)){
+
+
+    echo fgets($file1) .  "<br>";
+}
+
+//fwrite
+$my_file = fopen("file1.txt", "w");
+
+$text = "computer programing";
+
+fwrite($my_file,$text);
+
+
+//w+(read and write mode)
+
+$my_file = fopen("data.txt", "w+");
+
+fwrite($my_file, "Data test 1");
+
+
+//a+
+$my_file = fopen("data.txt", "a+");
+
+fwrite($my_file, "dddddddfggd");
+
+?>
