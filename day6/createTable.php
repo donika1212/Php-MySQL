9e42c555b3356c76923f763b4f6689a73d3b42a8
Florjonosmani <floni.flori@gmail.com>
2024-12-07 14:51:56 +0100

    Add files via upload

---
+++ day6/createTable.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$host="localhost";
+$db = "database1";
+$user = "root";
+$pass = "";
+
+try {
+    $conn = new PDO ("mysql:host=$host;dbname=$db",$user, $pass);
+
+    $sql = "CREATE TABLE user (id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         username VARCHAR (30) NOT NULL,
+         password  VARCHAR (30) NOT NULL)";
+
+
+    $conn->exec($sql);
+
+    echo "Table created successfully";
+
+
+}catch(Exeption $e) {
+
+echo "Error creatin Table " . $e->getMessage();
+
+
+}
+
+?>
