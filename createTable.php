9103537381cb36adf342ab33e9105d91b96d2271
ArtiolThaqi <artiolthaqi06@gmail.com>
2024-12-07 14:50:31 +0100

    Add files via upload

---
+++ createTable.php
@@ -0,0 +1,28 @@
+<?php
+
+$host = "localhost";
+$db = "database1";
+$user = "root";
+$pass = "";
+
+try{
+
+    $conn = new PDO ("mysql:host=$host;dbname=$db", $user,$pass);
+
+    $sql = "CREATE TABLE user (id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                             username VARCHAR(30) NOT NULL,
+                             password VARCHAR(50) NOT NULL )";
+
+    $conn -> exec($sql);
+
+    echo "Table created sucesfully";
+
+
+
+}catch (Exeption $e) {
+      
+    echo "Error creating table:" . $e ->getMessage();
+}
+
+
+?>
