1d661944478c64653e9f01486c38f310ad785816
Amar-Simnica <amarsimnica15@gmail.com>
2024-12-14 14:41:07 +0100

    Add files via upload

---
+++ index_GET.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action = "index_GET.php" method="get">
+
+<label for="username">Username:</label><br>
+<input type = "text" name="username" placeholder="Username"><br>
+<label for="password">Password:</label><br>
+<input type = "password" name="password" placeholder="Password"><br>
+<input type="submit" value="Submit"><br><br>
+
+
+
+<?php
+
+$username = $_GET["username"];
+$password = $_GET["password"];
+
+echo $username;
+
+echo"<br>";
+
+echo $password;
+
+?>
+    
+</body>
+</html>
