b4737f14d8a9666d7706db275dec01105298f272
AjanSs <ajansyla2@gmail.com>
2024-12-14 14:41:24 +0100

    Add files via upload

---
+++ mod9.2/index_GET.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index_GET.php" method="get">
+
+    <label for="username">Username:</label><br>
+    <input type="text" name="username" placeholder="username"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" name="password" placeholder="Password"><br><br>
+    <input type="submit" value="Submit"><br><br>
+
+</form>
+
+<?php
+$username = $_GET["username"];
+$password = $_GET["password"];
+
+echo $username;
+
+echo "<br>";
+
+echo $password;
+?>
+
+</body>
+</html>
