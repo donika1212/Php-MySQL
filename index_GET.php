8b7b4df0cddf8bf33d378f795b64f0e2221876d0
frasher165 <frashergashi5@gmail.com>
2024-12-14 14:42:58 +0100

    Add files via upload

---
+++ index_GET.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action = "index_GET.php" method ="get">
+
+<label for= "username">Username:</label>
+<input type="text" name ="usename"  placeholder="username"><br>
+<label for="password">password:</label><br>
+<input type= "password" name="password" placeholder="password"><br><br>
+<input type= "submit" value="submit"><br><br>
+
+</form>
+
+<?php
+$username = $_GET["username"];
+$password = $_GET["username"];
+
+echo $username;
+
+echo "<br>";
+
+echo $password;
+
+?>
+  
+</body>
+</html>
