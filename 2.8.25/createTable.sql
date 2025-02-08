55b409751c29e40248ead125c7305edb86c5a49e
LiridonaBaliu <75494050+LiridonaBaliu@users.noreply.github.com>
2025-02-08 16:16:11 +0100

    Delete 2.8.25 directory

--- 2.8.25/createTable.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE `products` (
-  `id` int(100) NOT NULL  AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `quantity` int(100) NOT NULL,
-  `price` int(100) NOT NULL
-);
