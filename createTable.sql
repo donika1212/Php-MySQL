392f1dfb1a7f8da7db0c508a22e172298237bbb4
LiridonaBaliu <75494050+LiridonaBaliu@users.noreply.github.com>
2025-02-08 13:14:30 +0100

    Add files via upload

---
+++ createTable.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL  AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `price` int(100) NOT NULL
+);
