6303f5e7557a3a5c0d8d574ab13cc6a8e2637b0f
frasher165 <frashergashi5@gmail.com>
2025-02-15 14:50:00 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,25 +1,21 @@
-
 <?php
-session_start();
 
-$user="root";
-$pass="";
-$server="localhost";
-$dbname="test";
+$user= "root";
+$pass= "";
+$server= "localhost";
+$dbname= "mms2";
 
 
 try {
-    
-    $conn =new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
+
+$conn = new PDO("mysql:host=$server; dbname=$dbname" ,$user , $pass);
 
 
+}catch(PDOExeption $e){
 
-} catch (PDOException $e) {
-    echo "error: " . $e->getMessage();
+echo "error:" . $e->getMessage();
+
 }
 
 
-
-
-
- ?>
+?>
--- index.php
+++ index.php
@@ -1,45 +1,98 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+	<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+	<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+	<style>
 
+		html,
+	body {
+	  height: 100%;
+	}
 
-    <style>
+	body {
+	  display: flex;
+	  align-items: center;
+	  padding-top: 40px;
+	  padding-bottom: 40px;
+	  background-color: #f5f5f5;
+	}
 
+	.form-signin {
+	  width: 100%;
+	  max-width: 330px;
+	  padding: 15px;
+	  margin: auto;
+	}
 
-        form>input {
-            margin-bottom: 10px;
-            font-size: 20px;
-            padding: 5px;
-        }
+	.form-signin .checkbox {
+	  font-weight: 400;
+	}
 
+	.form-signin .form-floating:focus-within {
+	  z-index: 2;
+	}
 
-        button {
-            background: none;
-            border: none;
-            border: 1px solid black;
-            padding: 10px 40px;
-            font-size: 20px;
-            cursor: pointer;
-        }
-    </style>
+	.form-signin input[type="email"] {
+	  margin-bottom: -1px;
+	  border-bottom-right-radius: 0;
+	  border-bottom-left-radius: 0;
+	}
+
+	.form-signin input[type="password"] {
+	  margin-bottom: 10px;
+	  border-top-left-radius: 0;
+	  border-top-right-radius: 0;
+	}
+	.form-floating{
+		margin: 10px;
+	}
+	</style>
 </head>
-<body>
-    
-    <form action="add.php" method="POST">
-        
-        <input type="text" name="username" placeholder="Username"><br>
-        <input type="text" name="name" placeholder="Emri"><br>
-        <input type="text" name="surname" placeholder="Mbiemri"><br>
-        <input type="password" name="password" placeholder="Password"><br>
-        <input type="email" name="email" placeholder="Email"><br><br>
-        <button type="submit" name="submit">Add</button>
+<body class="text-center">
+<!-- Creating a form which will post us some data in register.php file -->
+<main class="form-signin">
+  <form action="register.php" method="post">
+    <img class="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Register</h1>
 
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="emri" name="emri">
+      <label for="floatingInput">emri</label>
+    </div>
+    <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="username" name="username">
+      <label for="floatingInput">username</label>
+    </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="Email" name="email">
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingInput" placeholder="Password" name="password">
+      <label for="floatingInput">Password</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password" name="confirm_password">
+      <label for="floatingPassword">Confirm Password</label>
+    </div>
 
-    </form>
-
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Sign up</button>
+    <span>Already have an account: </span><a href="login.php">Sign in</a>
+  </form>
+</main>
 
 </body>
 </html>
