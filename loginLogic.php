7a62547a2270c9844b2af937d32dcaf69b2f4416
AjanSs <ajansyla2@gmail.com>
2025-02-22 14:02:31 +0100

    Add files via upload

--- loginLogic.php
+++ loginLogic.php
@@ -1,34 +1,49 @@
-<?php 
+<?php
 
-require 'config.php';
+session_start();
 
-if(isset($_POST['submit']))
-{
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+include_once('config.php');
 
-  if(empty($username) || empty($password))
-  {
-    echo "Fill all the fields!";
-    header( "refresh:2; url=login.php" ); 
-  }else{
-    $sql = "SELECT * FROM users WHERE username=:username";
-    $insertSql = $conn->prepare($sql);
-    $insertSql->bindParam(':username', $username);
+if(issset($_POST["submit"])) {
 
-    $insertSql->execute();
-    
-    if($insertSql->rowCount() > 0) {
-        $data=$insertSql->fetch();
-        if(password_verify($password,$data['password'])){
-          $_SESSION['username']=$data['username'];
-          header("Location: dashboard.php");
-        }else{
-          echo "Password incorrect";
-          header( "refresh:2; url=login.php" );
+    $username - $_POST['username'];
+    $password - $_POST['passsword'];
+
+    if(empty($username) || empty($password) ){
+
+        echo "Please fill all the fields.";
+    }else {
+
+        $sql = "SELECT id , name, username, email, password, confirm_password, is_admin, surname, FROM user1 where username = :username";
+
+        $selectUsers = $conn->prepare($sql);
+
+        $selectUsers->bindParam(":username", $username);
+
+        $selectUsers->execute();
+
+        $data = $selectUsers ->fetch();
+
+        id($data == false) {
+            echo "The user does not exist";
+        } else {
+
+            if(password_verify($password,$data["password"])){
+
+                $_SESSION['id'] = $data['id'];
+                $_SESSION['name'] = $data['name'];
+                $_SESSION['username'] = $data['username'];
+                $_SESSION['email'] = $data['email'];
+                $_SESSION['is_admin'] = $data['is_admin'];
+
+                header('Location: dashboard.php');
+            }else {
+                echo "The password is not valid";
+            }
         }
-    } else {
-        echo "User not found!!";
     }
-  }
 }
+
+
+
+?>
