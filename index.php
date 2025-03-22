189292895a4e7009b00fe44b4bf217d2dab25573
harisshalaku <130074412+harisshalaku@users.noreply.github.com>
2025-03-22 14:47:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,98 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-	<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
-	<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
-	<style>
-
-		html,
-	body {
-	  height: 100%;
-	}
-
-	body {
-	  display: flex;
-	  align-items: center;
-	  padding-top: 40px;
-	  padding-bottom: 40px;
-	  background-color: #f5f5f5;
-	}
-
-	.form-signin {
-	  width: 100%;
-	  max-width: 330px;
-	  padding: 15px;
-	  margin: auto;
-	}
-
-	.form-signin .checkbox {
-	  font-weight: 400;
-	}
-
-	.form-signin .form-floating:focus-within {
-	  z-index: 2;
-	}
-
-	.form-signin input[type="email"] {
-	  margin-bottom: -1px;
-	  border-bottom-right-radius: 0;
-	  border-bottom-left-radius: 0;
-	}
-
-	.form-signin input[type="password"] {
-	  margin-bottom: 10px;
-	  border-top-left-radius: 0;
-	  border-top-right-radius: 0;
-	}
-	.form-floating{
-		margin: 10px;
-	}
-	</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register & Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
-<body class="text-center">
-<!-- Creating a form which will post us some data in register.php file -->
-<main class="form-signin">
-  <form action="register.php" method="post">
-    <img class="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Register</h1>
+<body>
+    <div class="container" id="signup">
+        <h1 class="form-title">Register</h1>
+        <form method="post" action="">
+            <div class="input-group"></div>
+            <i class="fas fa-user></i>
+            <input type="text" name="fName" id="fName" placeholder="First name" required>
+            <label for="fname">First Name</label>
+</div>
+<div class="input-group">
+    <i class "fas fa-user"></i>
+    <input type="text" name="1Name" id="1Name" placeholder="Last Name" required>
+    <label for="1Name">Last Name</label>
+</div>
+<div class="input-group">
+    <i class="fas fa=envelope"></i>
+    <input type="email" name="email"  id="email"  placeholder="Email" require>
+    <label for="email">Email</label>
+</div>
+<div class="input-group">
+    <iclass="fas fa-lock"></i>
+    <input type="password" id="password" placeholder="password" required>
+    <label for="password">password</label>
+</div>
+<input type="submit" class="btn" value="Sign up" name="sign up">
 
-    <div class="form-floating">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Name" name="emri">
-      <label for="floatingInput">Name</label>
-    </div>
-    <div class="form-floating">
-      <input type="text" class="form-control" id="floatingInput" placeholder="Lastname" name="lastname">
-      <label for="floatingInput">Lastname</label>
-    </div>
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="Email" name="email">
-      <label for="floatingInput">Email</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingInput" placeholder="Password" name="password">
-      <label for="floatingInput">Password</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password" name="confirm_password">
-      <label for="floatingPassword">Confirm Password</label>
-    </div>
+</form>
+<p class="or">
+    ----------or----------
+</p>
+<div class="icons">
+    <i class="faab fa-gogle"></i>
+    <i class="faab fa-facebook"></i>
+</div>
+<div class="links">
+    <p>Already Have Account ?</p>
+    <button id="signInButton">Sign in</button>
+</div>
+</div>
 
-    <div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
-    </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Sign up</button>
-    <span>Already have an account: </span><a href="login.php">Sign in</a>
-  </form>
-</main>
 
+<div class="container" id="signup">
+        <h1 class="form-title">Sign in</h1>
+        <form method="post" action="">
+            
+            <div class="input-group"></div>
+            <i class="fas fa-user></i>
+            <input type="text" name="fName" id="fName" placeholder="First name" required>
+            <label for="fname">First Name</label>
+</div>
+<div class="input-group">
+    <i class "fas fa-user"></i>
+    <input type="text" name="1Name" id="1Name" placeholder="Last Name" required>
+    <label for="1Name">Last Name</label>
+</div>
+<div class="input-group">
+    <i class="fas fa=envelope"></i>
+    <input type="email" name="email"  id="email"  placeholder="Email" require>
+    <label for="email">Email</label>
+</div>
+<div class="input-group">
+    <iclass="fas fa-lock"></i>
+    <input type="password" id="password" placeholder="password" required>
+    <label for="password">password</label>
+</div>
+<input type="submit" class="btn" value="Sign up" name="sign up">
+
+</form>
+<p class="or">
+    ----------or----------
+</p>
+<div class="icons">
+    <i class="faab fa-gogle"></i>
+    <i class="faab fa-facebook"></i>
+</div>
+<div class="links">
+    <p>Already Have Account ?</p>
+    <button id="signInButton">Sign in</button>
+</div>
+</div>
 </body>
 </html>
